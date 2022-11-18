f95ca388afaadc3194b5ad04e84dc299e3a1281c
Josué Hoenicka <josuee.vzla@gmail.com>
2022-11-18 15:43:04 -0300

    add: constructor

--- src/main/java/com/josuehoenicka/portfolio/model/People.java
+++ src/main/java/com/josuehoenicka/portfolio/model/People.java
@@ -5,4 +5,16 @@ import lombok.Setter;
 
 @Getter @Setter
 public class People {
+    private long id;
+    private String name;
+    private String lastname;
+    
+    public People(){
+    }
+    
+    public People(long id, String name, String lastname) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+    }
 }
