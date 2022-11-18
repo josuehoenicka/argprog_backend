72695b937f00ab2fbb93600bae640b9cdbf57507
Josué Hoenicka <josuee.vzla@gmail.com>
2022-11-18 14:38:41 -0300

    update: rename controller

--- src/main/java/com/josuehoenicka/portfolio/controller/Controller.java
+++ src/main/java/com/josuehoenicka/portfolio/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.josuehoenicka.portfolio.Controller;
+package com.josuehoenicka.portfolio.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
